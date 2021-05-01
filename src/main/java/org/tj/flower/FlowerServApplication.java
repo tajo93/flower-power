5ee0ba8350bc1fe0c25be692c11f7eb590b3b3e0
Tarek Jomaa <22871457+tajo93@users.noreply.github.com>
2021-05-01 02:53:03 +0200

    init spring

---
+++ src/main/java/org/tj/flower/FlowerServApplication.java
@@ -0,0 +1,13 @@
+package org.tj.flower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowerServApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowerServApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/tj/flower/FlowerServApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tj.flower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerServApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
